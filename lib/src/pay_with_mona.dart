055620673be63f68b90243ee81435b6dcec12d5d
Serticode <helpdeskforserticode@gmail.com>
2025-05-13 17:13:20 +0100

    Updated SDK to force passing needed details through Checkout Model.

--- lib/src/pay_with_mona.dart
+++ lib/src/pay_with_mona.dart
@@ -13,6 +13,7 @@ class PayWithMona {
     required Color primaryColor,
     required Color secondaryColor,
     required String phoneNumber,
+    required num amount,
     String? bvn,
   }) {
     final monaCheckOut = MonaCheckOut(
@@ -26,6 +27,7 @@ class PayWithMona {
       primaryColor: primaryColor,
       secondaryColor: secondaryColor,
       bvn: bvn,
+      amount: amount,
     );
 
     return PayWithMonaWidget(
